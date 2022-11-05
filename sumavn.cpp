2bf6a823753d103eacdf81ab02a3459726c9ba3f
MaribelJaramillo <113115009+MaribelJaramillo@users.noreply.github.com>
2022-11-05 10:30:02 -0500

    Create sumavn.cpp

---
+++ sumavn.cpp
@@ -0,0 +1,28 @@
+//SUMA DE VARIOS NUMEROS 
+//Creado por MaribelJaramillo
+#include<iostream>
+ using namespace std;                                                                                                                                            //creadoporJorkyProaño
+
+int main()
+
+  {
+float MEJQ_x,MEJQ_s=0.0;
+
+int MEJQ_i=0,MEJQ_l;
+
+cout<<"Ingrese el valor de MEJQ_l: ";cin>>MEJQ_l;                                                      
+do{
+
+cout<<"Ingrese el valor de MEJQ_x: ";
+
+cin>>MEJQ_x;
+
+MEJQ_i=MEJQ_i+1;
+
+MEJQ_s=MEJQ_s+MEJQ_x;
+
+}while(MEJQ_i<MEJQ_l);                                            
+cout<<"La suma de los numero fue: "<<MEJQ_s<<endl;     
+return 0;
+
+}
